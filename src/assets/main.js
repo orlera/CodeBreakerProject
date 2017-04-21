7088ef76d2bf4b56a3d13ed774d549df30306d23
Andrea Orler <andrea.orler@gmail.com>
2017-04-21 15:08:35 +0200

    Step1

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,44 @@
+Number.prototype.pad = function (size) {
+    var s = String(this);
+    while (s.length < (size || 2)) { s = "0" + s; }
+    return s;
+}
+
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+
+    if (validateInput(input.value)) {
+		if (answer.value == '' && attempt.value == '') { 
+		    setHiddenFields();
+		}
+		attempt.value = parseInt(attempt.value) + 1;
+	}
+	else {
+		return false;
+	}
 }
 
-//implement new functions here
+function setHiddenFields() {
+	let guess = Math.floor(Math.random() * 10000);
+	answer.value = guess.pad(4);
+	attempt.value = 0;
+}
+
+function setMessage(message) {
+	let messageLabel = document.getElementById('message');
+	messageLabel.innerHTML = message;
+}
+
+function validateInput(input) {
+	if (String(input).length == 4) {
+		setMessage('');
+		return true;
+	}
+	else {
+		setMessage("Guesses must be exactly 4 characters long.");
+		return false;
+	}
+}
