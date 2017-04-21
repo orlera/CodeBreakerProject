2e12ceb7c47c74cf68c938af577f50ea7a051fee
Andrea Orler <andrea.orler@gmail.com>
2017-04-21 18:59:14 +0200

    step2

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,11 +10,15 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
 
+	console.log('guessing');
     if (validateInput(input.value)) {
+		console.log('valid input');
 		if (answer.value == '' && attempt.value == '') { 
 		    setHiddenFields();
+			console.log('hidden set');
 		}
 		attempt.value = parseInt(attempt.value) + 1;
+		getResult(input.value);
 	}
 	else {
 		return false;
@@ -41,4 +45,27 @@ function validateInput(input) {
 		setMessage("Guesses must be exactly 4 characters long.");
 		return false;
 	}
+}
+
+function getResult(input) {
+	let result = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+	let char_res = '';
+	let ans = answer.value;
+	console.log('ans' + ans);
+	console.log('res' + result);
+	console.log('len' + result.length);
+	for (let i = 0; i < input.length; i++) {
+		if (input[i] == ans[i]) {
+			char_res = '<span class="glyphicon glyphicon-ok"></span>';
+		} else if (input[i] == ans[0] || input[i] == ans[1] || input[i] == ans[2] || input[i] == ans[3]) {
+			char_res = '<span class="glyphicon glyphicon-transfer"></span>';
+		}
+		else {
+			char_res = '<span class="glyphicon glyphicon-remove"></span>'
+		}
+		result += char_res;
+	}
+	result += '</div></div>';
+
+	document.getElementById('results').innerHTML = result;
 }
