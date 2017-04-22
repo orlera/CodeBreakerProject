b3fdf63fd39817e1aa88a8885af0aa43e8cb2825
Andrea Orler <andrea.orler@gmail.com>
2017-04-22 11:24:47 +0200

    step3

--- src/assets/main.js
+++ src/assets/main.js
@@ -18,7 +18,18 @@ function guess() {
 			console.log('hidden set');
 		}
 		attempt.value = parseInt(attempt.value) + 1;
-		getResults(input.value);
+		if (getResults(input.value)) {
+			setMessage("You Win! :)");
+		}
+		else {
+			if (parseInt(attempt.value) >= 10) {
+				setMessage("You Lose! :(");
+			}
+			else {
+				setMessage("Incorrect, try again.");
+			}
+
+		}
 	}
 	else {
 		return false;
@@ -51,21 +62,23 @@ function getResults(input) {
 	let result = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
 	let char_res = '';
 	let ans = answer.value;
-	console.log('ans' + ans);
-	console.log('res' + result);
-	console.log('len' + result.length);
+	let correct = true;
 	for (let i = 0; i < input.length; i++) {
 		if (input[i] == ans[i]) {
 			char_res = '<span class="glyphicon glyphicon-ok"></span>';
 		} else if (input[i] == ans[0] || input[i] == ans[1] || input[i] == ans[2] || input[i] == ans[3]) {
 			char_res = '<span class="glyphicon glyphicon-transfer"></span>';
+			correct = false;
 		}
 		else {
-			char_res = '<span class="glyphicon glyphicon-remove"></span>'
+			char_res = '<span class="glyphicon glyphicon-remove"></span>';
+			correct = false;
 		}
 		result += char_res;
 	}
 	result += '</div></div>';
 
+	result = document.getElementById('results').innerHTML + result;
 	document.getElementById('results').innerHTML = result;
+	return correct;
 }
